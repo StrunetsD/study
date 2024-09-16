8112e367c4804a079eb65498da492907e504a0fb
Strunets Dmitry <144937226+StrunetsD@users.noreply.github.com>
2024-09-17 00:10:49 +0300

    Update 3.py

--- 12_lesson/3.py
+++ 12_lesson/3.py
@@ -6,7 +6,7 @@ def get_key(d, value):
 
 class Bus:
     
-    def __init__(self,max_seats = 15):
+    def __init__(self, max_seats=15):
         self.__speed = 60
         self.__max_seats = max_seats
         self.__max_speed = 90
@@ -14,13 +14,11 @@ class Bus:
         self.__free_place = max_seats
         self.__places = {}
 
-    def fixSpeed(self,value):
+    def fixSpeed(self, value):
         if value > 0:
-             self.__speed + value
+            self.__speed += value
         elif value < 0:
-            self.__speed - value
-        else :
-            self.__speed
+            self.__speed += value  # Уменьшаем скорость
         return self.__speed
     
     def landingDel(self, surnames):
@@ -29,31 +27,64 @@ class Bus:
                 key = get_key(self.__places, surname)  
                 if key is not None:
                     del self.__places[key] 
+                    self.__free_place += 1  # Увеличиваем количество свободных мест
 
-    def list_of_surnames(self,surnames):
-        for surname in surnames:
-            self.__surnames.append(surname)
+    def list_of_surnames(self):
         return self.__surnames
          
-    def places(self,surnames):
-        position = 1
-        for surname in surnames:
+    def places(self, surname):
+        if self.__free_place > 0:
+            position = next((i for i in range(1, self.__max_seats + 1) if i not in self.__places), None)
+            if position:
                 self.__places[position] = surname
-                position+=1
+                self.__surnames.append(surname)
+                self.__free_place -= 1
+        else:
+            print("Нет свободных мест!")
         return self.__places
+
+    def display_places(self):
+        if not self.__places:
+            print("Нет занятых мест.")
+        else:
+            print("Занятые места:")
+            for place, surname in self.__places.items():
+                print(f"Место {place}: {surname}")
+    
     def get_places(self):
         return self.__places
+
 def main():
     bus = Bus()
-    surnames = ["vova", "dima"]
-    landing = ["vova"]
-    print(bus.fixSpeed(-10))
-
-    print("Список фамилий:", bus.list_of_surnames(surnames))
-    bus.places(surnames)  
-    print("Места перед посадкой:", bus.get_places())
-    bus.landingDel(landing)  
-    print("Места после посадки:", bus.get_places())
+    while True:
+        print("""
+    1. Исправить скорость 
+    2. Удалить человека
+    3. Список фамилий
+    4. Добавить человека 
+    5. Показать занятые места
+    0. Exit
+          """)
+        choice = input("Выберите действие: ")
+        if choice == "1":
+            value = int(input("Введите значение для изменения скорости: "))
+            print(f"Текущая скорость: {bus.fixSpeed(value)}")
+        elif choice == "2":
+            surname = input("Введите фамилию для удаления: ")
+            bus.landingDel([surname])  # Передаем список
+            print(f"Фамилия '{surname}' удалена.")
+        elif choice == "3":
+            print("Список фамилий:", bus.list_of_surnames())
+        elif choice == "4":
+            surname = input("Введите фамилию: ")
+            bus.places(surname)
+            print(f"Фамилия '{surname}' добавлена.")
+        elif choice == "5":
+            bus.display_places()  # Вывод занятых мест
+        elif choice == "0":
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, попробуйте снова.")
 
 if __name__ == "__main__":
     main()
