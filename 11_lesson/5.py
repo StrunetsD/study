9dad0ce0db967ab371d158f5b77af4cb329f47a5
Strunets Dmitry <144937226+StrunetsD@users.noreply.github.com>
2024-09-17 20:43:31 +0300

    Update 5.py

--- 11_lesson/5.py
+++ 11_lesson/5.py
@@ -1,27 +1,22 @@
 class SuperStr(str):
-
-    def __init__(self, current):
-        self.current = current
-
-    def is_palindrom(self, string):
-        return string == string[::-1]
+    
+    def is_palindrom(self):
+        return self == self[::-1]
 
     def is_repeatance(self, s):
-        repeats = len(self.current) // len(s)
-        if repeats * s == self.current:
-            return True
-        else:
-            return False
+        repeats = len(self) // len(s)
+        return repeats * s == self
 
 
 def main():
-    super_str = SuperStr("bacasdf")
+    super_str = SuperStr("abcabcabc")
 
     if super_str.is_repeatance("abc"):
         print(True)
     else:
         print(False)
-    if super_str.is_palindrom("abccba"):
+
+    if super_str.is_palindrom():
         print(True)
     else:
         print(False)
